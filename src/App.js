474e44c5d3627bab52dd3d071767231706c07feb
Gergely Chikan <gc392@cornell.edu>
2020-07-20 13:13:05 +0200

    Added multiple grid formats

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Panel from './Panel.js';
 import dog from './images/dog.jpg';
-import { Container } from '@material-ui/core';
+import { Container, Grid, Typography, Box } from '@material-ui/core';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@material-ui/lab';
+
+var json = require('./info.json');
 
 const examplejson = {
   image: dog,
@@ -22,12 +24,82 @@ const examplejson = {
   ]
 }
 
+console.log(json);
+
 export default class App extends Component {
+
+  getSections = () => {
+    return (
+      json.map(section => {
+        return <Section json={section}/>;
+      })
+    )
+  }
+
   render() {
     return (
       <Container maxWidth="sm">
-        <Panel json={examplejson}/>
+        {this.getSections()}
       </Container>
     );
+  }
+}
+
+
+
+class Section extends Component {
+
+  getTimelinePanel = (panel) => {
+    const { panels } = this.props.json;
+    return (
+      <TimelineItem>
+        {panel.date == null ? <div /> : <TimelineOppositeContent><Typography variant="subtitle2">{panel.date}</Typography></TimelineOppositeContent>}
+        <TimelineSeparator>
+          <TimelineDot color="primary"/>
+          {panels[panels.length - 1] === panel ? <div /> : <TimelineConnector/>}
+        </TimelineSeparator>
+        <TimelineContent><Panel json={panel} /></TimelineContent>
+      </TimelineItem>
+    )
+  }
+
+  getPanels = () => {
+    const { panels, type } = this.props.json;
+    return (
+      panels.map(panel => {
+        return (
+          type === "timeline" ? this.getTimelinePanel(panel) :
+          type === "medium" ?
+          <Grid xs={12} sm={6} item><Panel json={panel} /></Grid> :
+          type === "small" ?
+          <Grid xs={6} sm={4} item><Panel json={panel} /></Grid> :
+          <Grid xs={12} item><Panel json={panel} /></Grid>
+        );
+      })
+    )
+  }
+
+  getHeader = () => {
+    const { name } = this.props.json;
+    return (
+      <Typography align="center" variant="h5">{name}</Typography>
+    )
+  }
+
+  render() {
+    const { name, type } = this.props.json;
+    return (
+      <React.Fragment>
+      {this.getHeader()}
+      {
+        type === "timeline" ?
+        <Timeline>
+          {this.getPanels()}
+        </Timeline> :
+        <Grid container alignItems="stretch" spacing={3}>
+          {this.getPanels()}
+        </Grid>
+      }</React.Fragment>
+    )
   }
 }
