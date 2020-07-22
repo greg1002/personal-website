57d169a1c81cec39d5f733780d1ad1a5e9219cf0
Gergely Chikan <gc392@cornell.edu>
2020-07-22 14:29:51 +0200

    Added icons, pdfs, centered links

--- src/Panel.js
+++ src/Panel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardContent, CardActions, CardMedia, Typography, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-//import { theme } from 'theme.js';
+import { Card, CardContent, CardActions, CardMedia, Typography, Button, Grid, Box } from '@material-ui/core';
+import { School, Work, Business, Code, Error } from '@material-ui/icons';
 
 /*
 JSON FORMAT:
@@ -29,6 +28,34 @@ export default class Panel extends Component {
     />
   }
 
+  getIcon = (type) => {
+    switch(type) {
+      case "school": return <School/>
+      case "work": return <Work />
+      case "business": return <Business />
+      case "code": return <Code />
+      default: return <Error />
+    }
+  }
+
+  getCardIcons = (icons) => {
+    return icons == null ? <div /> :
+    <Grid container>{
+        icons.map(icon => {
+          return (
+            <Grid item xs={12} sm={6}>
+              <Box p={1} display="flex" alignItems="center">
+                {this.getIcon(icon.type)}
+                <Box pl={2}>
+                  <Typography variant="body2">{icon.text}</Typography>
+                </Box>
+              </Box>
+            </Grid>
+          )
+        })
+      }</Grid>
+  }
+
   getCardContent = (json) => {
     return (
       <CardContent>
@@ -45,6 +72,9 @@ export default class Panel extends Component {
           </Typography>
         }
         {
+          json.icons == null ? <div /> : this.getCardIcons(json.icons)
+        }
+        {
           json.body == null ? <div /> :
           <Typography variant="body2" component="p">
             {json.body}
@@ -56,17 +86,19 @@ export default class Panel extends Component {
 
   getCardActions = (json) => {
     return json.links == null || json.links.length === 0 ? <div /> :
-    <CardActions>
+    <CardActions><Grid container>
       {
         json.links.map(link => {
           return (
-            <Button href={link.href} size="small" color="primary">
+            <Grid item xs={12 / json.links.length}>
+              <Button href={link.href} size="small" fullWidth="true" color="primary">
               {link.name}
-            </Button>
+              </Button>
+            </Grid>
           )
         })
       }
-    </CardActions>
+    </Grid></CardActions>
   }
 
   render() {
